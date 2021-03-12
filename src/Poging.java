1db93133a26ae328bc93fd40aeae8d76b869e48c
Stutterinman <terencio_12@live.nl>
2021-03-12 10:53:58 +0100

    Update Poging.java

--- src/Poging.java
+++ src/Poging.java
@@ -5,5 +5,25 @@ class Poging {
     private Examen examen;
     private Student student;
 
-    
+    public Poging(Student student, Examen examen){
+        //Nog ff leeglaten
+    }
+    public Student getStudent(){
+        //Return de naam van de student
+        return this.student;
+    }
+    public Examen getExamen(){
+        //Return de naam van het examen
+        return this.examen;
+    }
+    public Boolean getGeslaagd(){
+        //Kijk of de poging is geslaagd
+
+    }
+    public ArrayList<Student> hoogsteScore(){
+        //Nog ff leeglaten
+    }
+    public void examenAfname(){
+
+    }
 }
