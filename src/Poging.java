7a23374d8037d3afecca5a16b685416c10ed98f7
Stutterinman <terencio_12@live.nl>
2021-03-12 10:40:57 +0100

    Update Poging.java

---
+++ src/Poging.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+class Poging {
+    private Boolean geslaagd;
+    public static ArrayList<Poging> pogingenLijst = new ArrayList<Poging>();
+    private Examen examen;
+    private Student student;
+
+    
+}
