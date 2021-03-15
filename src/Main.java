944ce5f26e09497b8e1da7cebefb1c1b5848179a
InvisibileRiri <74571959+InvisibileRiri@users.noreply.github.com>
2021-03-15 12:03:08 +0100

    :blink: examen array en nested array

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,39 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String[] examen1vragen =
+                {
+                        "Je rijdt op de rechterrijstrook van een autosnelweg. Door de verkeersdrukte hebben bestuurders moeite met invoegen. Wat doe je?",
+                        "De motor van je voertuig is nog koud. Je wilt gaan rijden. Wanneer verbruik je minder brandstof?",
+                        "Wat is het alarmnummer in Nederland?",
+                        "Welke voertuigen moet je binnen de bebouwde kom voor laten gaan als deze bij een bushalte willen wegrijden?",
+                        "Je hebt te veel gedronken en je veroorzaakt een ongeluk. Kan een verzekeringsmaatschappij zeggen dat je de schade zelf moet betalen?"
+                };
+
+        String[][] examen1opties =
+                {
+                {
+                        "Je blijft met dezelfde snelheid op de rechterrijstrook rijden.",
+                        "Je gaat langzamer rijden en schruigt op naar de linkerrijstrook als er ruimte is.",
+                        "Je gaat naar de linkerrijstrook als er ruimte is of je gaat langzamer rijden."
+                }, {
+                        "Als je meteen rustig wegrijdt, nadat je de koude motor heb gestart.",
+                        "Als je eerst de motor warm laat worden en daarna pas gaat rijden.",
+                        "Dat maakt voor het brandstofverbruik niet uit."
+                }, {
+                        "112.",
+                        "06-112.",
+                        "911."
+                }, {
+                        "Alleen lijnbussen.",
+                        "Alleen touringcars.",
+                        "Autobussen."
+                }, {
+                        "JA",
+                        "NEE"
+                }
+        };
+
+
     }
 }
