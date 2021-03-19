ba73853cdd7edcffe37424511df0488fd9e94695
hhs-20170173 <74719192+hhs-20170173@users.noreply.github.com>
2021-03-19 14:52:38 +0100

    Vraag afgemaakt

---
+++ src/Vraag.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class Vraag{
+
+
+    private String toetsVraag;
+    private String antwoord;
+    private ArrayList<String> opties;
+
+    public Vraag(String toetsVraag, String[] opties, String antwoord){
+        this.toetsVraag = toetsVraag;
+        this.antwoord = antwoord;
+
+        this.opties = new ArrayList<>();
+
+        for (int i = 0; i < opties.length; i++) {
+            this.opties.add(opties[i]);
+        }
+    }
+    public void schudOpties() {
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int randint = random.nextInt(3);
+
+            String holder = opties.get(randint);
+            opties.remove(randint);
+            opties.add(holder);
+        }
+    }
+
+
+    public String getToetsVraag() {
+        return this.toetsVraag;
+    }
+
+    public ArrayList<String> getOpties(){
+        return opties;
+    }
+
+
+    public String getAntwoord() {
+        return this.antwoord;
+    }
+
+
+
+}
