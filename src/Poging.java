be05eb7159c83119e7d2327c37cfa5a8b9964ccc
hhs-20170173 <74719192+hhs-20170173@users.noreply.github.com>
2021-03-19 14:34:19 +0100

    poging afgemaakt

--- src/Poging.java
+++ src/Poging.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 class Poging {
     private Boolean geslaagd;
     public static ArrayList<Poging> pogingenLijst;
@@ -21,20 +23,87 @@ class Poging {
     }
     public Boolean getGeslaagd(){
         //Kijk of de poging is geslaagd
-        return examen.getVoldoende();
+        return this.geslaagd;
     }
-    //public static ArrayList<Student> hoogsteScore(){
-        //Nog ff leeglaten
-    //}
+    public static ArrayList<Student> hoogsteScore(){
+        ArrayList studenten = new ArrayList();
+        ArrayList<Integer> score = new ArrayList();
+
+        for (Poging poging: getPogingenLijst()) {
+            if (!studenten.contains(poging.getStudent())) {
+                if (poging.getGeslaagd()) {
+                    score.add(1);
+                }
+            } else {
+                if (poging.getGeslaagd()) {
+                    int index = studenten.indexOf(poging.getStudent());
+                    int punten = score.get(index) + 1;
+                    score.set(index, punten);
+                }
+            }
+        }
+
+        int highestScore = 0;
+        for (int i : score) {
+            if (i > highestScore) {
+            highestScore = i;
+            }
+        }
+        ArrayList besteStudenten = new ArrayList();
+        int j = 0;
+        for (int i : score) {
+
+            if (i == highestScore) {
+                besteStudenten.add(studenten.get(j));
+            }
+            j++;
+            }
+
+        return besteStudenten;
+    }
 
     public void examenAfname(){
+        Scanner scan = new Scanner(System.in);
+
         int questionGoed = 0;
-        for(examen.getExamenLijst) {
-            system.out.println(examen.getExamenLijst);
+        char abc = 'A';
+
+        for(Vraag vraag: examen.getVragenLijst()) {
+            System.out.println();
+            System.out.println("Vraag: " + vraag.getToetsVraag());
+
+            for (String optie : vraag.getOpties()) {
+                System.out.println(abc + ": " + optie);
+                abc++;
+            }
+            System.out.print("Antwoord: ");
+            String response = scan.nextLine();
+
+            if (response.equals("A")) {
+                if (vraag.getOpties().get(0).equals(vraag.getAntwoord())) {
+                    questionGoed++;
+                }
+            }
+            else if (response.equals("B")) {
+                if (vraag.getOpties().get(1).equals(vraag.getAntwoord())) {
+                    questionGoed++;
+                }
+            }
+            else if (response.equals("C")) {
+                if (vraag.getOpties().get(2).equals(vraag.getAntwoord())) {
+                    questionGoed++;
+                }
+            }
+        }
+
+        if (questionGoed >= Examen.voldoende) {
+            geslaagd = true;
+        } else {
+            geslaagd = false;
         }
     }
-    public static void getPogingenLijst(){
-        return this.pogingenLijst;
+    public static ArrayList<Poging> getPogingenLijst() {
+        return pogingenLijst;
     }
 
 }
