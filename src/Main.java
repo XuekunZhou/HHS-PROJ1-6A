e1e541c55f53208bd2426d6309876e07447c7ebc
InvisibileRiri <74571959+InvisibileRiri@users.noreply.github.com>
2021-03-23 11:46:15 +0100

    Update Main.java
    :100: added more questions and other test.

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,12 @@ public class Main {
                         "De motor van je voertuig is nog koud. Je wilt gaan rijden. Wanneer verbruik je minder brandstof?",
                         "Wat is het alarmnummer in Nederland?",
                         "Welke voertuigen moet je binnen de bebouwde kom voor laten gaan als deze bij een bushalte willen wegrijden?",
-                        "Je hebt te veel gedronken en je veroorzaakt een ongeluk. Kan een verzekeringsmaatschappij zeggen dat je de schade zelf moet betalen?"
+                        "Je hebt te veel gedronken en je veroorzaakt een ongeluk. Kan een verzekeringsmaatschappij zeggen dat je de schade zelf moet betalen?",
+                        "Wat kun je in het verkeer beter niet doen?",
+                        "Bij een verkeerslicht stopt een auto naast je. De bestuurder geeft de hele tijd gas om als eerste weg te rijden als het groen wordt. Wat doe je?",
+                        "Je wilt een noodstop maken met een auto met een ABS. Hoe kun je het beste remmen?",
+                        "Wanneer mag je tijdens een autorit whatsappen of sms'en?",
+                        "Je rijdt op de motorrijstrook van een autosnelweg. Door de verkeersdrukte hebben bestuurders moeite met invoegen. Wat doe je?"
                 };
 
         String[][] examen1opties =
@@ -32,6 +37,24 @@ public class Main {
                 }, {
                         "JA",
                         "NEE"
+                }, {
+                        "Harde muziek draaien met de ramen open en snel optrekken bij een verkeerslicht.",
+                        "Harde muziek draaien met de ramen op en toeteren in een gevaarlijke situatie."
+                }, {
+                        "Je doet niet mee met het spel en je rijdt normaal bij het verkeerslicht weg.",
+                        "Je probeert sneller weg te zijn dan de andere bestuurder."
+                }, {
+                        "Je trapt zo stevig mogelijk op de rem en houd deze vast tot je stilstaat.",
+                        "Je trapt de rem stevig in en zodra je tegendruk voelt van het rempedaal, laat je de rem langzaam een beetje los.",
+                        "Je gaat pompend remmen waarbij je de rem telkens even loslaat."
+                }, {
+                        "Nooit.",
+                        "Als je het andere verkeer niet in gevaar brengt.",
+                        "Als je stilstaat voor een geopende brug of sluis."
+                }, {
+                        "Je blijft met dezelfde snelheid op de rechterrijstrook rijden.",
+                        "Je gaat naar de linkerrijstrook als er ruimte is of je gaat langzamer rijden.",
+                        "Je gaat langzamer rijden en schuift op naar de linkerrijstrook als er ruimte is."
                 }
         };
 
@@ -41,7 +64,12 @@ public class Main {
                         "Wanneer kun je de controle over je auto verliezen?",
                         "Je rijdt op een smalle weg. Er staan bomen dicht langs de kant van de weg. Welke bewering is juist?",
                         "Je bent op een autosnelweg aan het inhalen. Je ziet een auto hard aan komen rijden. Hij gaat vlak achter je rijden. Wat moet je doen?",
-                        "Het heeft gesneeuwd. Wat doen je voordat je instapt?"
+                        "Het heeft gesneeuwd. Wat doen je voordat je instapt?",
+                        "Wanneer mag je niet inhalen?",
+                        "De lading op je voertuig is 2.50m breed. Mag je hiermee op een verharde weg rijden?",
+                        "Welke bewering is waar?",
+                        "Waarop moet je extra alert zijn in een landelijke omgeving?",
+                        "Je wilt inhalen op een rechte weg. In de verte is er sprake van tegemoetkomend verkeer. Waarmee moet je rekening houden?"
                 };
 
         String[][] examen2opties =
@@ -49,67 +77,101 @@ public class Main {
                         {
                                 "NEE",
                                 "JA"
-                        },
-                        {
+                        }, {
                                 "Als je moe bent of alcohol hebt gebruikt.",
                                 "Als je handsfree aan het bellen bent.",
                                 "Als je in het donker lange tijd achter elkaar een bepaalde snelheid rijdt."
-                        },
-                        {
+                        }, {
                                 "Door de tegenliggers en de bomen kun je niet uitwijken.",
                                 "Door de bomen kun je de weg goed zien. Je hoeft je snelheid dus niet aan te passen.",
                                 "Je kunt afstanden goed inschatten door het licht en de schaduw van de bomen."
-                        },
-                        {
+                        }, {
                                 "Je gaat door met inhalen, als je hard genoeg rijdt. Daarna ga je meteen naar rechts.",
                                 "Je trapt even op de rem. Zo geef je aan dat de auto meer afstand moet houden.",
                                 "Je gaat langzamer rijden. Je wilt namelijk dat de bestuurder achter je meer afstand houdt."
-                        },
-                        {
+                        }, {
                                 "Je haalt de sneeuw van de ruiten, de lichten, de spiegels, de kentekenplaat en het dak.",
                                 "Je haalt de sneeuw van de lichten en zet de raamverwarming aan.",
                                 "Je veegt je auto helemaal schoon en laat de motor alvast warm worden."
+                        }, {
+                                "Wanneer de afstand die nodig is om in te halen niet vrij is  of niet volledig is te overzien.",
+                                "Wanneer je rechts van een blokmarkering rijdt.",
+                                "Wanneer je niet minimaal 30 km/h sneller kunt rijden dan het in te halen voertuig.",
+                        }, {
+                                "JA",
+                                "NEE"
+                        }, {
+                                "Een personenauto mag maximaal 12 meter lang zijn.",
+                                "Een personenauto mag maximaal 8 meter lang zijn.",
+                                "Voor personenauto's is geen maximum lengte bepaald."
+                        }, {
+                                "Vervuilde rijbanen.",
+                                "Windvlagen.",
+                                "Brede rijbanen waarop hard gereden wordt."
+                        }, {
+                                "Je kunt moeilijk inschatten wat de snelheid is van het tegemoetkomend verkeer.",
+                                "De bestuurder van het in te halen voertuig schrikt van het tegemoetkomend verkeer."
                         }
-
-
-
         };
 
         String[] examen3vragen =
                 {
-                        "Wat kun je in het verkeer beter niet doen?",
-                        "Bij een verkeerslicht stopt een auto naast je. De bestuurder geeft de hele tijd gas om als eerste weg te rijden als het groen wordt. Wat doe je?",
-                        "Je wilt een noodstop maken met een auto met een ABS. Hoe kun je het beste remmen?",
-                        "Wanneer mag je tijdens een autorit whatsappen of sms'en?",
-                        "Je rijdt op de motorrijstrook van een autosnelweg. Door de verkeersdrukte hebben bestuurders moeite met invoegen. Wat doe je?"
+                        "Wat is 12x12?",
+                        "Wat is 190:10?",
+                        "Wat is 417:139?",
+                        "Wat is 588:84?",
+                        "Wat is 718-190?",
+                        "Wat is 915:15?",
+                        "Wat is 864:4?",
+                        "Wat is 928:8?",
+                        "Wat is 354:59?",
+                        "Wat is 690+160?"
                 };
 
         String[][] examen3opties =
                 {
                         {
-                                "Harde muziek draaien met de ramen open en snel optrekken bij een verkeerslicht.",
-                                "Harde muziek draaien met de ramen op en toeteren in een gevaarlijke situatie."
+                                "144",
+                                "124",
+                                "112"
+                        }, {
+                                "19",
+                                "10",
+                                "109"
+                        }, {
+                                "3",
+                                "5",
+                                "9"
                         },
                         {
-                                "Je doet niet mee met het spel en je rijdt normaal bij het verkeerslicht weg.",
-                                "Je probeert sneller weg te zijn dan de andere bestuurder."
-
-
+                                "7",
+                                "10",
+                                "11"
                         },
                         {
-                                "Je trapt zo stevig mogelijk op de rem en houd deze vast tot je stilstaat.",
-                                "Je trapt de rem stevig in en zodra je tegendruk voelt van het rempedaal, laat je de rem langzaam een beetje los.",
-                                "Je gaat pompend remmen waarbij je de rem telkens even loslaat."
-                        },
-                        {
-                                "Nooit.",
-                                "Als je het andere verkeer niet in gevaar brengt.",
-                                "Als je stilstaat voor een geopende brug of sluis."
-                        },
-                        {
-                                "Je blijft met dezelfde snelheid op de rechterrijstrook rijden.",
-                                "Je gaat naar de linkerrijstrook als er ruimte is of je gaat langzamer rijden.",
-                                "Je gaat langzamer rijden en schuift op naar de linkerrijstrook als er ruimte is."
+                                "528",
+                                "524",
+                                "526"
+                        }, {
+                                "61",
+                                "59",
+                                "64"
+                        }, {
+                                "216",
+                                "210",
+                                "218"
+                        }, {
+                                "116",
+                                "114",
+                                "112"
+                        }, {
+                                "6",
+                                "7",
+                                "9"
+                        }, {
+                                "850",
+                                "870",
+                                "855"
                         }
                 };
     }
