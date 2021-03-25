20a41d244090298fabb8841ac27c7d74e2846d8b
Stutterinman <terencio_12@live.nl>
2021-03-25 13:33:14 +0100

    Update PogingTest.java

--- test/PogingTest.java
+++ test/PogingTest.java
@@ -1,13 +1,17 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+<<<<<<< Updated upstream
 import static org.junit.Assert.assertEquals;
+=======
+>>>>>>> Stashed changes
 import static org.junit.jupiter.api.Assertions.*;
 
 class PogingTest {
 
     @Test
     void hoogsteScore() {
+<<<<<<< Updated upstream
     Student student1 = new Student("Jan", "Smit");
     Student student2 = new Student("Bob", "Rob");
     Poging poging = new Poging(student1);
@@ -18,6 +22,18 @@ class PogingTest {
     studenten.add(student1);
 
     Assert.assertEquals(studenten, Poging.hoogsteScore());
+=======
+        Student student1 = new Student("Jan", "Smit");
+        Student student2 = new Student("Bob", "Rob");
+        Poging poging = new Poging(student1);
+        Poging poging2 = new Poging(student1);
+        Poging poging3 = new Poging(student2);
+
+        ArrayList<Student> studenten = new ArrayList();
+        studenten.add(student1);
+
+        Assert.assertEquals(studenten, Poging.hoogsteScore());
+>>>>>>> Stashed changes
     }
 
     @Test
