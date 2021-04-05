9a18d14f8c3ab4a58f98f9f79ead2cbb771756fc
samsorultra <samsor13@icloud.com>
2021-04-05 12:30:20 +0200

    aanpassingen samsor

--- src/Poging.java
+++ src/Poging.java
@@ -98,16 +98,25 @@ class Poging {
                 case "A":
                     if (vraag.getOpties().get(0).equals(vraag.getAntwoord())) {
                         questionGoed = questionGoed + 2;
+                        System.out.println("Correct!");
+                    } else {
+                        System.out.println("Incorrect!");
                     }
                     break;
                 case "B":
                     if (vraag.getOpties().get(1).equals(vraag.getAntwoord())) {
                         questionGoed = questionGoed + 2;
+                        System.out.println("Correct!");
+                    } else {
+                        System.out.println("Incorrect!");
                     }
                     break;
                 case "C":
                     if (vraag.getOpties().get(2).equals(vraag.getAntwoord())) {
                         questionGoed = questionGoed + 2;
+                        System.out.println("Correct!");
+                    } else {
+                        System.out.println("Incorrect!");
                     }
                     break;
             }
